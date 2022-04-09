67f87cc97f0c9979f663c11fc886c44ccf22cbef
pratik-k <prakar07@protonmail.com>
2022-04-09 22:40:37 +0530

    make dropdown btn with evt listener

--- dist/main.js
+++ dist/main.js
@@ -15,7 +15,7 @@
   \**********************/
 /***/ (() => {
 
-eval("console.log(\"🪄\")\n\n//# sourceURL=webpack://dropdown-menu/./src/index.js?");
+eval("function turnToHtml(str){\n    let temp_div=document.createElement(\"div\")\n    temp_div.innerHTML=str\n    return temp_div.querySelector(\"*:nth-child(1)\")\n}\n\nfunction addDropDownBtn(){\n    const btn=turnToHtml(`<button class=\"drop-down-btn\">bruh</button>`)\n    btn.style.height=\"30px\"\n    btn.style.position=\"relative\"\n    btn.addEventListener(\"mouseenter\",addDropdown)\n    btn.addEventListener(\"mouseleave\",deleteDropdown)\n    document.body.appendChild(btn)\n}\n\nfunction addDropDownLinks(dropdownElem,linksObj){\n    console.log(linksObj)\n    linksObj={\"yo\":\"https://google.com\"}\n    for(const [name,link] of Object.entries(linksObj)){\n        dropdownElem.appendChild(turnToHtml(`\n        <li class=\"dropdown-link\"><a href=\"${link}\">${name}</a></li>\n        `))\n    }\n}\n\n\nfunction addDropdown(evt){\n    dropdownElem=turnToHtml(`<ul class=\"dropdown\"></ul>`)\n    addDropDownLinks(dropdownElem)\n    dropdownElem.style.position=\"absolute\"\n    dropdownElem.style.right=0\n    dropdownElem.style.bottom=10\n    evt.target.appendChild(dropdownElem)\n    addDropDownLinks(dropdownElem,{\"yo\":\"https://google.com\"})\n}\n\nfunction deleteDropdown(){\n    // document.querySelector(\".dropdown\").remove()\n}\n\n\naddDropDownBtn()\n\n//# sourceURL=webpack://dropdown-menu/./src/index.js?");
 
 /***/ })
 
--- src/index.js
+++ src/index.js
@@ -1,1 +1,42 @@
-console.log("🪄")
+function turnToHtml(str){
+    let temp_div=document.createElement("div")
+    temp_div.innerHTML=str
+    return temp_div.querySelector("*:nth-child(1)")
+}
+
+function addDropDownBtn(){
+    const btn=turnToHtml(`<button class="drop-down-btn">bruh</button>`)
+    btn.style.height="30px"
+    btn.style.position="relative"
+    btn.addEventListener("mouseenter",addDropdown)
+    btn.addEventListener("mouseleave",deleteDropdown)
+    document.body.appendChild(btn)
+}
+
+function addDropDownLinks(dropdownElem,linksObj){
+    console.log(linksObj)
+    linksObj={"yo":"https://google.com"}
+    for(const [name,link] of Object.entries(linksObj)){
+        dropdownElem.appendChild(turnToHtml(`
+        <li class="dropdown-link"><a href="${link}">${name}</a></li>
+        `))
+    }
+}
+
+
+function addDropdown(evt){
+    dropdownElem=turnToHtml(`<ul class="dropdown"></ul>`)
+    addDropDownLinks(dropdownElem)
+    dropdownElem.style.position="absolute"
+    dropdownElem.style.right=0
+    dropdownElem.style.bottom=10
+    evt.target.appendChild(dropdownElem)
+    addDropDownLinks(dropdownElem,{"yo":"https://google.com"})
+}
+
+function deleteDropdown(){
+    // document.querySelector(".dropdown").remove()
+}
+
+
+addDropDownBtn()
