8aebc26f462a5be88f36a972382b7538a557b310
pratik-k <prakar07@protonmail.com>
2022-04-10 16:23:43 +0530

    add delete dropdown evt and style.css

--- dist/main.js
+++ dist/main.js
@@ -13,20 +13,96 @@
 /*!**********************!*\
   !*** ./src/index.js ***!
   \**********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _style_css__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./style.css */ \"./src/style.css\");\n/* harmony import */ var _style_css__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_style_css__WEBPACK_IMPORTED_MODULE_0__);\n\n\n\nfunction turnToHtml(str){\n    let temp_div=document.createElement(\"div\")\n    temp_div.innerHTML=str\n    return temp_div.querySelector(\"*:nth-child(1)\")\n}\n\nfunction addDropDownBtn(){\n    const btn=turnToHtml(`<button class=\"drop-down-btn\">bruh</button>`)\n    btn.style.height=\"30px\"\n    btn.style.position=\"relative\"\n    btn.addEventListener(\"mouseover\",addDropdown)\n    btn.addEventListener(\"mouseleave\",deleteDropdown)\n    document.body.appendChild(btn)\n}\n\nfunction addDropDownLinks(dropdownElem,linksObj){\n    console.log(linksObj)\n    // linksObj={\"yo\":\"https://google.com\"}\n    for(const [name,link] of Object.entries(linksObj)){\n        dropdownElem.appendChild(turnToHtml(`\n        <li class=\"dropdown-link\"><a href=\"${link}\">${name}</a></li>\n        `))\n    }\n}\n\n\nfunction addDropdown(evt){\n    dropdownElem=turnToHtml(`<ul class=\"dropdown\"></ul>`)\n    dropdownElem.style.position=\"absolute\"\n    dropdownElem.style.right=0\n    dropdownElem.style.bottom=10\n    evt.target.appendChild(dropdownElem)\n    console.log(\"addDropDownBtn\")\n    addDropDownLinks(dropdownElem,{\"yo\":\"https://google.com\",\"bo\":\"github.com\"})\n}\n\nfunction deleteDropdown(){\n    document.querySelector(\".dropdown\").remove()\n}\n\n\naddDropDownBtn()\n\n//# sourceURL=webpack://dropdown-menu/./src/index.js?");
+
+/***/ }),
+
+/***/ "./src/style.css":
+/*!***********************!*\
+  !*** ./src/style.css ***!
+  \***********************/
 /***/ (() => {
 
-eval("function turnToHtml(str){\n    let temp_div=document.createElement(\"div\")\n    temp_div.innerHTML=str\n    return temp_div.querySelector(\"*:nth-child(1)\")\n}\n\nfunction addDropDownBtn(){\n    const btn=turnToHtml(`<button class=\"drop-down-btn\">bruh</button>`)\n    btn.style.height=\"30px\"\n    btn.style.position=\"relative\"\n    btn.addEventListener(\"mouseenter\",addDropdown)\n    btn.addEventListener(\"mouseleave\",deleteDropdown)\n    document.body.appendChild(btn)\n}\n\nfunction addDropDownLinks(dropdownElem,linksObj){\n    console.log(linksObj)\n    linksObj={\"yo\":\"https://google.com\"}\n    for(const [name,link] of Object.entries(linksObj)){\n        dropdownElem.appendChild(turnToHtml(`\n        <li class=\"dropdown-link\"><a href=\"${link}\">${name}</a></li>\n        `))\n    }\n}\n\n\nfunction addDropdown(evt){\n    dropdownElem=turnToHtml(`<ul class=\"dropdown\"></ul>`)\n    addDropDownLinks(dropdownElem)\n    dropdownElem.style.position=\"absolute\"\n    dropdownElem.style.right=0\n    dropdownElem.style.bottom=10\n    evt.target.appendChild(dropdownElem)\n    addDropDownLinks(dropdownElem,{\"yo\":\"https://google.com\"})\n}\n\nfunction deleteDropdown(){\n    // document.querySelector(\".dropdown\").remove()\n}\n\n\naddDropDownBtn()\n\n//# sourceURL=webpack://dropdown-menu/./src/index.js?");
+eval("\n\n//# sourceURL=webpack://dropdown-menu/./src/style.css?");
 
 /***/ })
 
 /******/ 	});
 /************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/compat get default export */
+/******/ 	(() => {
+/******/ 		// getDefaultExport function for compatibility with non-harmony modules
+/******/ 		__webpack_require__.n = (module) => {
+/******/ 			var getter = module && module.__esModule ?
+/******/ 				() => (module['default']) :
+/******/ 				() => (module);
+/******/ 			__webpack_require__.d(getter, { a: getter });
+/******/ 			return getter;
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
 /******/ 	
 /******/ 	// startup
 /******/ 	// Load entry module and return exports
 /******/ 	// This entry module can't be inlined because the eval devtool is used.
-/******/ 	var __webpack_exports__ = {};
-/******/ 	__webpack_modules__["./src/index.js"]();
+/******/ 	var __webpack_exports__ = __webpack_require__("./src/index.js");
 /******/ 	
 /******/ })()
 ;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+import "./style.css"
+
+
 function turnToHtml(str){
     let temp_div=document.createElement("div")
     temp_div.innerHTML=str
@@ -8,14 +11,14 @@ function addDropDownBtn(){
     const btn=turnToHtml(`<button class="drop-down-btn">bruh</button>`)
     btn.style.height="30px"
     btn.style.position="relative"
-    btn.addEventListener("mouseenter",addDropdown)
+    btn.addEventListener("mouseover",addDropdown)
     btn.addEventListener("mouseleave",deleteDropdown)
     document.body.appendChild(btn)
 }
 
 function addDropDownLinks(dropdownElem,linksObj){
     console.log(linksObj)
-    linksObj={"yo":"https://google.com"}
+    // linksObj={"yo":"https://google.com"}
     for(const [name,link] of Object.entries(linksObj)){
         dropdownElem.appendChild(turnToHtml(`
         <li class="dropdown-link"><a href="${link}">${name}</a></li>
@@ -26,16 +29,16 @@ function addDropDownLinks(dropdownElem,linksObj){
 
 function addDropdown(evt){
     dropdownElem=turnToHtml(`<ul class="dropdown"></ul>`)
-    addDropDownLinks(dropdownElem)
     dropdownElem.style.position="absolute"
     dropdownElem.style.right=0
     dropdownElem.style.bottom=10
     evt.target.appendChild(dropdownElem)
-    addDropDownLinks(dropdownElem,{"yo":"https://google.com"})
+    console.log("addDropDownBtn")
+    addDropDownLinks(dropdownElem,{"yo":"https://google.com","bo":"github.com"})
 }
 
 function deleteDropdown(){
-    // document.querySelector(".dropdown").remove()
+    document.querySelector(".dropdown").remove()
 }
 
 
